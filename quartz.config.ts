6f852c6b8924f933cd54f6b2d5fbb5e7d09697be
Manuel Terreno <manuelterreno@192.168.1.54>
2026-03-09 18:27:04 +0100

    Quartz sync: Mar 9, 2026, 6:27 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -56,6 +56,9 @@ const config: QuartzConfig = {
   plugins: {
     transformers: [
       Plugin.FrontMatter(),
+      Plugin.CreatedModifiedDate({
+        priority: ["frontmatter", "git", "filesystem"],
+      }),
       Plugin.SyntaxHighlighting({
         theme: {
           light: "github-light",
