18e1b70a84dd020797deee4ef8fa92d42461cb94
Manuel Terreno <manuelterreno@192.168.1.54>
2026-03-09 18:49:19 +0100

    Quartz sync: Mar 9, 2026, 6:49 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -44,7 +44,7 @@ beforeBody: [
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle()],
+  beforeBody: [Component.Breadcrumbs(),
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
