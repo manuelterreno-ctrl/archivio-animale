8c4954ed5e3b13b08ec684de062f3d5ae834b18f
Manuel Terreno <manuelterreno@192.168.1.54>
2026-03-09 17:10:36 +0100

    Quartz sync: Mar 9, 2026, 5:10 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -16,15 +16,14 @@ export const sharedPageComponents: SharedLayout = {
 
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
-  beforeBody: [
-    Component.ConditionalRender({
-      component: Component.Breadcrumbs(),
-      condition: (page) => page.fileData.slug !== "index",
-    }),
-    Component.ArticleTitle(),
-    Component.ContentMeta(),
-    Component.TagList(),
-  ],
+beforeBody: [
+  Component.ConditionalRender({
+    component: Component.Breadcrumbs(),
+    condition: (page) => page.fileData.slug !== "index",
+  }),
+  Component.ArticleTitle(),
+  Component.TagList(),
+],
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
@@ -49,7 +48,7 @@ export const defaultContentPageLayout: PageLayout = {
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
+  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle()],
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
